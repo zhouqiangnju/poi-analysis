21084628c04138e382811bbf26365aae10ec06b3
zhouqiangnju <zhouqiangnju@163.com>
2018-11-21 17:32:01 +0800

    2018_11_21_new

--- poi-analysis/code/poi_analysis_nj.R
+++ poi-analysis/code/poi_analysis_nj.R
@@ -8,6 +8,9 @@ jq_poly=readRDS('~/GitHub/data/data_universal/geometry_of_js_highrate_jq/jq_poly
 nj_sf=readRDS('~/GitHub/data/poi_analysis/nj_district_sf.rds') %>% st_transform(4509)
 x=st_intersects(nj_poi,nj_sf)%>%lapply(length) %>% unlist
 poi=nj_poi[x==1,]
+
+
+
 #make ppp files
 #make owin for whole city of nanjing
 nj_city_owin=nj_sf %>% as('Spatial') %>% as('SpatialPolygons') %>% as('owin')
@@ -20,9 +23,28 @@ for(i in 1:nrow(nj_sf))
 nj_district_owin = nj_district%>%tess(tiles=. )
 #make ppp file for poi
 poi_coord=st_coordinates(poi)
-marks_df=dplyr::select(poi,adcode,type1,type2) 
+marks_df=dplyr::select(poi,adcode,maintype,subtletype) 
 st_geometry(marks_df)=NULL
 poi_ppp=ppp(x=poi_coord[,1],y=poi_coord[,2],window=nj_city_owin,marks = marks_df)
+
+test=sample(poi$maintype,200) 
+t=factor(test,levels = c('010000','020000','030000','040000','050000','060000','070000','080000','090000',
+                         '100000','110000','120000','130000','140000','150000','160000','170000','180000'))
+t
+# new data input
+jq_poly=readRDS('~/GitHub/data/data_universal/geometry_of_js_highrate_jq/jq_polygon.rds')  %>% st_transform(4509)
+nj_sf=readRDS('~/GitHub/data/poi_analysis/nj_district_sf.rds') %>% st_transform(4509)
+poi=readRDS('F:/Administrator/Documents/GitHub/poi-analysis/poi-analysis/poi_for_ppp.rds')
+poi_ppp=readRDS('F:/Administrator/Documents/GitHub/poi-analysis/poi-analysis/nj_poi_ppp.rds')
+poi$maintype=str_extract(poi$type1,'^\\d{2}') %>% paste0('0000')
+poi$subtletype=str_extract(poi$type2,'^\\d{2}') %>% paste0('0000')
+poi$subtletype=factor(poi$subtletype,levels=c('010000','020000','030000','040000','050000','060000','070000','080000','090000',
+                                          '100000','110000','120000','130000','140000','150000','160000','170000','180000',NA),exclude = NULL)
+poi$maintype %>% class
+table(poi$subtletype)
+x=poi[poi$maintype=='010000',]
+
+
 quadratcount.ppp(poi_ppp,tess=nj_district_owin)
 poi_ppp$marks$adcode=factor(poi_ppp$marks$adcode)
 summary(poi_ppp)
@@ -41,5 +63,3 @@ persp(den,theta=30)
 ?spatstat::persp.im()
 contour(den,axes=F)
 aden=adaptive.density(poi_ppp,f=0.01,nrep=10)
-library(spatstat.utils)
-ad
---
+++ poi-analysis/code/poi_type_clean.R
@@ -0,0 +1,50 @@
+#poi type clea
+library('doParallel')
+nj_entity_poi=readRDS('~/GitHub/data/poi_analysis/nj_entity_poi(type).rds') 
+type=nj_entity_poi$typecode 
+prefix_og=type %>% str_extract('^\\d{2}')
+type_list=type  %>% str_split('\\|')
+which(type_number==3)
+type_number=lapply(type_list,length) %>% unlist
+
+type_extract=function(typecode){
+   type_number = typecode %>% str_split('\\|') %>%unlist%>% length  
+   type_list   = typecode %>% str_split('\\|')  %>%unlist
+   if(type_number>1){
+    prefix_table=type_list %>% str_extract('^\\d{2}') %>% table %>% as.data.frame
+    
+     if(prefix_table[1,2]>1){
+     type1=prefix_table[1,1] %>% paste0('^',.)   %>% str_subset(type_list,.) %>% '['(1)
+     type2=prefix_table[2,1] %>% paste0('^',.)   %>% str_subset(type_list,.) %>% '['(1)
+     }else{
+      type1=type_list[1]
+      type2=type_list[type_list!=type1][1]
+    }
+  }   else{
+     type1=type_list
+     type2=NA
+   }
+   type=cbind(type1,type2) 
+}
+x=foreach(i=1:nrow(nj_entity_poi),.combine = rbind) %dopar% type_extract(nj_entity_poi$typecode[i]) 
+
+type_add=map(poi$typecode,type_extract)
+type_add=rlist::list.rbind(type_add)
+type_add_sf=as.data.frame(type_add)
+poi_add=cbind(poi,type_add_sf)
+poi$type2=type_add_sf$type2
+which(type_add_sf$type1=='010000')
+table(type_add_sf$type1)
+poi$typecode[159334]
+x=type_extract(poi$typecode[7605])
+foreach(i=1:nrow(type_number)) %dopar% 
+?foreach
+vignette("nested") 
+
+
+
+
+cl<-makeCluster(4)  
+registerDoParallel(cl)  
+stopImplicitCluster()
+stopCluster()
